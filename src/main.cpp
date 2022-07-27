2f70fd3b949ad0a4dea881cf9e14095e2167c92d
Olive <lexicon99999@gmail.com>
2022-07-27 16:42:26 -0700

    weekday, working chaser clock, color scheme

--- src/main.cpp
+++ src/main.cpp
@@ -94,75 +94,82 @@ const bool nums[10][15] = {
      1, 1, 1}};
 
 const bool days[8][30] = {
-  { // Er (error)
-    1, 1, 1, 0, 0, 0,
-    1, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1,
-    1, 0, 0, 1, 0, 0,
-    1, 1, 1, 1, 0, 0
-  }, { // Su
-    1, 1, 1, 0, 0, 0,
-    1, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 0, 1,
-    0, 0, 1, 1, 0, 1,
-    1, 1, 1, 1, 1, 1
-  }, { // M
-    1, 1, 1, 1, 1, 0,
-    1, 0, 1, 0, 1, 0,
-    1, 0, 1, 0, 1, 0,
-    1, 0, 1, 0, 1, 0,
-    1, 0, 1, 0, 1, 0
-  }, { // Tu
-    1, 1, 1, 0, 0, 0,
-    0, 1, 0, 0, 0, 0,
-    0, 1, 0, 1, 0, 1,
-    0, 1, 0, 1, 0, 1,
-    0, 1, 0, 1, 1, 1
-  }, { // W
-    1, 0, 1, 0, 1, 0,
-    1, 0, 1, 0, 1, 0,
-    1, 0, 1, 0, 1, 0,
-    1, 0, 1, 0, 1, 0,
-    1, 1, 1, 1, 1, 0
-  }, { // Th
-    1, 1, 1, 1, 0, 0,
-    0, 1, 0, 1, 0, 0,
-    0, 1, 0, 1, 1, 1,
-    0, 1, 0, 1, 0, 1,
-    0, 1, 0, 1, 0, 1
-  }, { // Fr
-    1, 1, 1, 0, 0, 0,
-    1, 0, 0, 0, 0, 0,
-    1, 1, 0, 1, 1, 1,
-    1, 0, 0, 1, 0, 0,
-    1, 0, 0, 1, 0, 0
-  }, { // Sa
-    1, 1, 1, 0, 0, 0,
-    1, 0, 0, 0, 0, 1,
-    1, 1, 1, 1, 1, 1,
-    0, 0, 1, 1, 0, 1,
-    1, 1, 1, 1, 1, 1
-  }
+    {1, 1, 1, 0, 0, 0, // Er (error)
+     1, 0, 0, 0, 0, 0,
+     1, 1, 1, 1, 1, 1,
+     1, 0, 0, 1, 0, 0,
+     1, 1, 1, 1, 0, 0},
+    {1, 1, 1, 0, 0, 0, // Su
+     1, 0, 0, 0, 0, 0,
+     1, 1, 1, 1, 0, 1,
+     0, 0, 1, 1, 0, 1,
+     1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 0, // M
+     1, 0, 1, 0, 1, 0,
+     1, 0, 1, 0, 1, 0,
+     1, 0, 1, 0, 1, 0,
+     1, 0, 1, 0, 1, 0},
+    {1, 1, 1, 0, 0, 0, // Tu
+     0, 1, 0, 0, 0, 0,
+     0, 1, 0, 1, 0, 1,
+     0, 1, 0, 1, 0, 1,
+     0, 1, 0, 1, 1, 1},
+    {1, 0, 1, 0, 1, 0, // W
+     1, 0, 1, 0, 1, 0,
+     1, 0, 1, 0, 1, 0,
+     1, 0, 1, 0, 1, 0,
+     1, 1, 1, 1, 1, 0},
+    {1, 1, 1, 1, 0, 0, // Th
+     0, 1, 0, 1, 0, 0,
+     0, 1, 0, 1, 1, 1,
+     0, 1, 0, 1, 0, 1,
+     0, 1, 0, 1, 0, 1},
+    {1, 1, 1, 0, 0, 0, // Fr
+     1, 0, 0, 0, 0, 0,
+     1, 1, 0, 1, 1, 1,
+     1, 0, 0, 1, 0, 0,
+     1, 0, 0, 1, 0, 0},
+    {1, 1, 1, 0, 0, 0, // Sa
+     1, 0, 0, 0, 0, 1,
+     1, 1, 1, 1, 1, 1,
+     0, 0, 1, 1, 0, 1,
+     1, 1, 1, 1, 1, 1}};
+
+const uint32_t minuteColors[15] = {
+    strip.Color(255, 12 , 0  ), // 0 red
+    strip.Color(237, 36 , 0  ), // 1 
+    strip.Color(219, 73 , 0  ), // 2 
+    strip.Color(200, 109, 0  ), // 3 
+    strip.Color(164, 146, 0  ), // 4 
+    strip.Color(109, 182, 0  ), // 5 
+    strip.Color(55 , 200, 0  ), // 6 green
+    strip.Color(36 , 219, 0  ), // 7 
+    strip.Color(18 , 255, 36 ), // 8 
+    strip.Color(36 , 219, 73 ), // 9 
+    strip.Color(55 , 164, 128), // 10
+    strip.Color(73 , 109, 200), // 11
+    strip.Color(91 , 73 , 255), // 12
+    strip.Color(109, 36 , 191), // 13
+    strip.Color(128, 12 , 128)  // 14
 };
 
-const uint32_t minuteColors[16] = {
-    strip.Color(0, 0, 0),       // black
-    strip.Color(250, 190, 212), // pink
-    strip.Color(230, 25, 75),   // red
-    strip.Color(128, 0, 0),     // maroon
-    strip.Color(170, 110, 40),  // brown
-    strip.Color(245, 130, 48),  // orange
-    strip.Color(255, 225, 25),  // yellow
-    strip.Color(255, 250, 200), // beige
-    strip.Color(170, 255, 195), // mint
-    strip.Color(60, 180, 75),   // green
-    strip.Color(0, 128, 128),   // teal
-    strip.Color(70, 240, 240),  // cyan
-    strip.Color(0, 130, 200),   // blue
-    strip.Color(0, 0, 128),     // navy
-    strip.Color(240, 50, 230),  // magenta
-    strip.Color(220, 190, 255)  // lavender
-};
+/* alternate 
+    strip.Color(26 , 0  , 0  ), // 0
+    strip.Color(77 , 26 , 0  ), // 1
+    strip.Color(128, 51 , 0  ), // 2
+    strip.Color(166, 64 , 0  ), // 3
+    strip.Color(255, 153, 0  ), // 4
+    strip.Color(230, 230, 0  ), // 5
+    strip.Color(128, 255, 0  ), // 6
+    strip.Color(51 , 230, 13 ), // 7
+    strip.Color(13 , 128, 26 ), // 8
+    strip.Color(0  , 89 , 51 ), // 9
+    strip.Color(0  , 64 , 64 ), // 10
+    strip.Color(26 , 51 , 77 ), // 11
+    strip.Color(51 , 26 , 128), // 12
+    strip.Color(77 , 0  , 204), // 13
+    strip.Color(77 , 0  , 255)  // 14
+*/
 
 int xyToIndex(short x, short y)
 {
@@ -172,7 +179,6 @@ int xyToIndex(short x, short y)
 void showDigit(short digit, int startX, int startY, uint32_t color)
 {
   for (int y = 0; y < 5; y++)
-  {
     for (int x = 0; x < 3; x++)
     {
       if (nums[digit][x + y * 3])
@@ -180,7 +186,19 @@ void showDigit(short digit, int startX, int startY, uint32_t color)
       else
         strip.setPixelColor(xyToIndex(startX + x, startY + y), strip.Color(0, 0, 0));
     }
-  }
+  strip.show();
+}
+
+void showDay(int startX, int startY, uint32_t color)
+{
+  for (int y = 0; y < 5; y++)
+    for (int x = 0; x < 6; x++)
+    {
+      if (days[Pacific.weekday()][x + y * 6])
+        strip.setPixelColor(xyToIndex(startX + x, startY + y), color);
+      else
+        strip.setPixelColor(xyToIndex(startX + x, startY + y), strip.Color(0, 0, 0));
+    }
   strip.show();
 }
 
@@ -188,6 +206,10 @@ void leftClock(uint32_t clockColor)
 {
   if (Pacific.hour() >= 10)
     showDigit(Pacific.hour() / 10, 0, 0, clockColor);
+  else
+    for (int y = 0; y < 5; y++)
+      for (int x = 0; x < 3; x++)
+        strip.setPixelColor(xyToIndex(x, y), strip.Color(0, 0, 0));
   showDigit(Pacific.hour() % 10, 4, 0, clockColor);
   strip.setPixelColor(65, clockColor);
   strip.setPixelColor(67, clockColor);
@@ -209,10 +231,10 @@ int timeToX()
   return (Pacific.hour() + stripClockOffset) % 24 * 4 + Pacific.minute() / 15;
 }
 
-void stripClock()
+void chaserClock()
 {
-  // strip.setPixelColor(xyToIndex(max(timeToX() - 1, 0), 7), minuteColors[0]);
-  strip.setPixelColor(xyToIndex(timeToX(), 7), minuteColors[Pacific.minute() / 4]);
+  strip.setPixelColor(xyToIndex(max(timeToX() - 1, 0), 7), strip.Color(0, 0, 0));
+  strip.setPixelColor(xyToIndex(timeToX(), 7), minuteColors[Pacific.minute() % 15]);
 }
 
 void setup(void)
@@ -263,8 +285,12 @@ void loop(void)
   }
   else
   {
-    leftClock(strip.Color(40, 7, 10));
-    stripClock();
+    leftClock(minuteColors[14]);
+    showDay(20, 0, minuteColors[14]);
+    // test color Pallette
+    // for (int x = 0; x < 15; x++)
+    //   strip.setPixelColor(xyToIndex(31 + x, 4), minuteColors[x]);
+    chaserClock();
     strip.show();
     delay(333);
   }
